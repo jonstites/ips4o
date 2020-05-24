fcd872882cef5dcb31ca1c18a4ee181bed620123
Jonathan Stites <mail@jonstites.com>
2020-05-24 12:27:21 -0500

    Clean up benchmarking code

--- benchmarks/speed.plot
+++ benchmarks/speed.plot
@@ -20,35 +20,28 @@ set yrange [0:4]
 set xlabel 'Item Count [log_2(n)]'
 set ylabel 'Run Time / n log_2n [Nanoseconds]'
 
-set style line 1 lt 1 lw .2
-set style line 2 lt 1 lw 1
 
-set style line 3 lt 2 lc 2 lw .2
-set style line 4 lt 1 lc 2 lw 1
-
-set style line 5 lt 2 lc 3 lw .2
-set style line 6 lt 1 lc 3 lw 1
-
-set style line 7 lt 2 lc 8 lw .2
-set style line 8 lt 1 lc 8 lw 1
+set style line 1 lt 1 lc 2 lw 1
+set style line 2 lt 1 lc 3 lw 1
+set style line 3 lt 1 lc 8 lw 1
 
 #SQL DELETE FROM stats WHERE LOG(2, size) < 10
 
-# most cross-DB way of doing what "SELECT ..., a FROM ..., (VALUES(-1), (0),
-# (1)) as dev (a)" does in postgres
-# SQL CREATE TABLE IF NOT EXISTS temp_add (a INTEGER)
-# SQL DELETE FROM temp_add
-# SQL INSERT INTO temp_add VALUES (-1), (0), (1)
-
-set title 'Random'
-## MULTIPLOT(algo, a) SELECT LOG(2, size) AS x,
-## AVG(time + a * stddev) / (size * log(2, size)) AS y,
+set title 'Random, i32'
+## MULTIPLOT(algo) SELECT LOG(2, size) AS x,
+## time / (size * log(2, size)) AS y,
 ## MULTIPLOT
-## FROM stats, temp_add WHERE dist = 'uniform' AND parallelism = 'true'
+## FROM stats WHERE dist = 'uniform' AND parallelism = 'true' AND type = 'i32'
 ## GROUP BY MULTIPLOT, size ORDER BY MULTIPLOT, size
 plot \
-    'speed-data.txt' index 0 title "algo=rayon::par_sort,a=-1" with linespoints, \
-    'speed-data.txt' index 1 title "algo=rayon::par_sort,a=0" with linespoints, \
-    'speed-data.txt' index 2 title "algo=rayon::par_sort,a=1" with linespoints
+    'speed-data.txt' index 0 title "par_sort" noenhanced with linespoints
 
-# SQL DROP TABLE temp_add
+
+set title 'Random, i64'
+## MULTIPLOT(algo) SELECT LOG(2, size) AS x,
+## time / (size * log(2, size)) AS y,
+## MULTIPLOT
+## FROM stats WHERE dist = 'uniform' AND parallelism = 'true' AND type = 'i64'
+## GROUP BY MULTIPLOT, size ORDER BY MULTIPLOT, size
+plot \
+    'speed-data.txt' index 1 title "par_sort" noenhanced with linespoints
--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -8,55 +8,6 @@ use std::ops::Add;
 
 use ips4o;
 
-/*
-fn gen_random<T>(rng: &mut T, len: usize) -> Vec<u32>
-where T: Rng 
-{
-    rng.sample_iter(&Standard).take(len).collect()
-}
-
-fn benchmark<T>(rng: &mut T, f: &fn(&mut T, usize) -> Vec<u32>, log_n: usize, iterations: usize)
-where T: Rng 
-{
-    let n = 1 << log_n;
-
-    let mut par_sort_times = Vec::new();
-    let mut par_sort_unstable_times = Vec::new();
-    let mut ips4o_times = Vec::new();
-
-    for _i in 0..iterations {
-        let v = gen_random(rng, n);
-
-        {
-            let mut v_copy = v.clone();
-            let now = Instant::now();
-            v_copy.par_sort();
-            let elapsed = now.elapsed();
-            par_sort_times.push(elapsed);
-        }
-
-        {
-            let mut v_copy = v.clone();
-            let now = Instant::now();
-            v_copy.par_sort_unstable();
-            let elapsed = now.elapsed();
-            par_sort_unstable_times.push(elapsed);
-        }
-
-        {
-            let mut v_copy = v.clone();
-            let now = Instant::now();
-            ips4o::sort(&mut v_copy);
-            let elapsed = now.elapsed();
-            ips4o_times.push(elapsed);
-        }
-
-    }
-    write_benchmark("par_sort", "random", n, iterations, par_sort_times);
-    write_benchmark("par_sort_unstable", "random", n, iterations, par_sort_unstable_times);
-    write_benchmark("ips4o", "random", n, iterations, ips4o_times);
-}*/
-
 fn mean(data: &[Duration]) -> Option<f32>
 {
     let sum = data.iter().sum::<Duration>().as_nanos() as f32;
@@ -68,25 +19,9 @@ fn mean(data: &[Duration]) -> Option<f32>
     }
 }
 
-fn std_deviation(data: &[Duration]) -> Option<f32> 
-{
-    match (mean(data), data.len()) {
-        (Some(data_mean), count) if count > 0 => {
-            let variance = data.iter().map(|value| {
-                let diff = data_mean - value.as_nanos() as f32;
-
-                diff * diff
-            }).sum::<f32>() / count as f32;
-
-            Some(variance.sqrt())
-        },
-        _ => None
-    }
-}
-
 trait GenData {
 
-    fn uniform(rng: &mut XorShiftRng, size: usize) -> Vec<Self>
+    fn uniform(rng: &mut XorShiftRng, len: usize) -> Vec<Self>
         where Self: Sized;
 
     fn label() -> String;
@@ -94,8 +29,8 @@ trait GenData {
 
 impl GenData for i32 {
 
-    fn uniform(rng: &mut XorShiftRng, size: usize) -> Vec<Self> {
-        vec!()
+    fn uniform(rng: &mut XorShiftRng, len: usize) -> Vec<Self> {
+        rng.sample_iter(&Standard).take(len).collect()
     }
 
     fn label() -> String {
@@ -105,8 +40,8 @@ impl GenData for i32 {
 
 impl GenData for i64 {
 
-    fn uniform(rng: &mut XorShiftRng, size: usize) -> Vec<Self> {
-        vec!()
+    fn uniform(rng: &mut XorShiftRng, len: usize) -> Vec<Self> {
+        rng.sample_iter(&Standard).take(len).collect()
     }
 
     fn label() -> String {
@@ -134,11 +69,11 @@ where T: GenData + Send + Ord, SortAlgo: Fn(&mut [T]), Dist: Fn(&mut XorShiftRng
         }
 
         println!(
-            "RESULT algo={} dist={} size={} iters={} time={} stddev={} parallelism={}", 
+            "RESULT algo={} dist={} size={} iters={} time={} parallelism={} type={}", 
             sorter_label, dist_label, len, ITERATIONS, 
-            mean(&times[..]).unwrap(), 
-            std_deviation(&times[..]).unwrap(),
-            parallelism
+            mean(&times[..]).unwrap(),
+            parallelism,
+            T::label()
         )
     }
 }
@@ -146,82 +81,12 @@ where T: GenData + Send + Ord, SortAlgo: Fn(&mut [T]), Dist: Fn(&mut XorShiftRng
 fn benchmark<T, SortAlgo>(mut sorter: SortAlgo, sorter_label: &str, parallelism: bool) 
 where T: GenData + Send + Ord, SortAlgo: Fn(&mut [T])
 {
-    /*let mut times = Vec::new();
-
-    for _i in 0..iterations {
-        let v = gen_random(rng, n);
-
-        {
-            let mut v_copy = v.clone();
-            let now = Instant::now();
-            v_copy.par_sort();
-            let elapsed = now.elapsed();
-            par_sort_times.push(elapsed);
-        }
-
-
-    println!(
-        "RESULT algo={} dist={} size={} iters={} time={} stddev={} parallelism={}", 
-        algo, dist, size, iterations, 
-        mean(&times[..]).unwrap(), 
-        std_deviation(&times[..]).unwrap(),
-        parallelism
-    );*/
-
     run(sorter, sorter_label, T::uniform, "uniform", parallelism);
-
 }
 
-fn benchmarks()
-{
-    /*const MAX_LOGN: usize = 26;
-    const ITERATIONS: usize = 10;
-    const SEED: u64 = 2020;
-
-    for logn in 0..MAX_LOGN {
-        {
-            let mut rng = XorShiftRng::seed_from_u64(SEED);
-            for _i in 0..ITERATIONS {
-                let mut v = T::uniform();
-                let f = rayon::slice::ParallelSliceMut::par_sort;
-                f(&mut v[..]);
-            }
-        }
-
-
-    }
-    
-    const MAX_LOG_N: usize = 26;
-    const ITERATIONS: usize = 10;
-    const SEED: u64 = 2020;
-
-    for log_n in 0..MAX_LOG_N {
-
-
-        let size = 1 << log_n;
-    
-    */
-
-    let algo = "rayon::par_sort";
+fn main() {
+    let sorter_label = "rayon::par_sort";
     let parallelism = true;
-    benchmark::<i32, _>(&rayon::slice::ParallelSliceMut::par_sort, algo, parallelism);
-    benchmark::<i64, _>(&rayon::slice::ParallelSliceMut::par_sort, algo, parallelism);
-    
-    
-}
-
-fn main() {    
-    /*let mut rng = XorShiftRng::seed_from_u64(2020);
-    const ITERATIONS: usize = 10;
-    const MAX_LOG_SIZE: usize = 26;
-
-    let distribution_gens: Vec<&dyn Fn(&mut XorShiftRng, usize) -> Vec<u32>> = vec![&gen_random];
-
-    for log_n in 0..=MAX_LOG_SIZE {
-        for f in &distribution_gens {
-            benchmark(&mut rng, f, log_n, ITERATIONS);
-        }
-    }*/    
-    benchmarks();
-
+    benchmark::<i32, _>(&rayon::slice::ParallelSliceMut::par_sort, sorter_label, parallelism);
+    benchmark::<i64, _>(&rayon::slice::ParallelSliceMut::par_sort, sorter_label, parallelism);
 }
